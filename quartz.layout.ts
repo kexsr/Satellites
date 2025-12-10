14615d3b878da23c85d3b92192624a1e92066a58
kexsr <sofa91166@gmail.com>
2025-12-10 23:39:07 +0700

    vault backup: 2025-12-10 23:39:07

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,6 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Автор-разработчик: Щедренко С.Р.": "",
     },
   }),
 }
@@ -41,7 +42,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
